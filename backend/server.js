c480bd33f712301931181dad46c7358a933bbf70
Saurya Gur <saurya.gur@outlook.com>
2025-01-19 23:32:02 +0530

    Updated todos

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 import express from "express";
-import cors from "cors";
+import cors from "cors"; // for api
+import env from "dotenv"; // env
+import passport from "passport"; // for auth
 import {Strategy as SpotifyStrategy} from "passport-spotify";
-import env from "dotenv";
-import passport from "passport";
+import session from "express-session"; // session handling
 
-//TODO: (Ishita) implement redis for session
+//TODO: (Ishita) implement mongodb as session-store
 
 const app = express();
 const port = process.env.APP_PORT || 3000;
@@ -14,6 +15,17 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 //for api
 app.use(cors());
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,  // Replace with a secure key
+    resave: false,
+    saveUninitialized: false,
+    // TODO: (Ishita) Use Mongodb for session store here
+}));
+
+// TODO: (Rehmatjot) Get passportjs auth working
+app.use(passport.initialize());
+app.use(passport.session());
 
 //env file is in repo root
 env.config({
@@ -26,7 +38,6 @@ passport.use(new SpotifyStrategy({
     clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
     callbackURL: `${process.env.APPURL}/auth/spotify/callback`
 }, function (accessToken, refreshToken, expires_in, profile, done) {
-    //TODO: (Siddhi) Should we add some more
     User.findOrCreate({spotifyId: profile.id}, function (err, user) {
         return done(err, user);
     });
@@ -44,19 +55,24 @@ app.get("/dashboard", (req, res) => {
 app.get(
     '/auth/spotify',
     passport.authenticate('spotify', {
-        //TODO: (Siddhi) add scope as discussed in meeting
         //https://developer.spotify.com/documentation/web-api/concepts/scopes#user-modify-playback-state
         scope: ['user-read-email', 'user-read-private']
     })
 );
 
-app.get('/auth/spotify/callback', passport.authenticate('spotify',
-        {failureRedirect: '/login'}),
-    function (req, res) {
-        // successful authentication, redirect to dashboard.
-        res.redirect('/dashboard');
+// TODO: (Rehmatjot) Add spotify callback endpoint get and set success and failiure redirect
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);  // Serialize user by their ID
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {  // Find user by ID
+        done(err, user);
     });
+});
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-})
+});
